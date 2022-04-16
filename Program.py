7c00643a5cb0d53fbe860154f681fc2f56034026
AlexeYakovlev <103351973+Username1212121246@users.noreply.github.com>
2022-04-16 10:41:43 +0400

    Add files via upload

---
+++ Program.py
@@ -0,0 +1,18 @@
+print ('Введите числа для примера 1')
+a=input()
+b=input()
+print ('Введите числа для примера 2')
+c=input()
+d=input()
+a=int(a)
+b=int(b)
+c=int(c)
+d=int(d)
+m=a+b
+n=c+d
+print ('Пример 1',m)
+print ('Пример 2',n)
+if m>n: print ('Пример 1 больше =',m)
+else: print ('Пример 2 больше =',n)
+
+
